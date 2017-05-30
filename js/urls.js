9a3681d763e63d3761814c5d186ed87e5f2479eb
Thô <thomas.bottini@gmail.com>
2017-05-30 12:59:21 +0200

    Pas de slash final dans la redirection (ça tuait les liens des actus)

--- js/urls.js
+++ js/urls.js
@@ -1,23 +1,6 @@
 var supported_langs = ['fr', 'en'];
 
-  var change_loc = function (lang) {
-    var location_parts = window.location.toString().split('/').filter(function (_) {
-      return _;
-    });
-    var lang_in_url = '';
-    if (supported_langs.indexOf(location_parts[2]) !== -1) {
-      lang_in_url = location_parts[2];
-      location_parts.splice(2, 1);
-    }
-
-    if (lang !== lang_in_url) {
-      location_parts.splice(2, 0, lang);
-      location_parts[0] += '//';
-      window.location = location_parts.join('/') + '/';
-    }
-  };
-
-  // Detect browser lang if there is no lang in the URL
+var change_loc = function (lang) {
   var location_parts = window.location.toString().split('/').filter(function (_) {
     return _;
   });
@@ -26,8 +9,25 @@ var supported_langs = ['fr', 'en'];
     lang_in_url = location_parts[2];
     location_parts.splice(2, 1);
   }
-  if (!lang_in_url) {
-    change_loc(navigator.language.startsWith('fr') ? 'fr' : 'en');
+
+  if (lang !== lang_in_url) {
+    location_parts.splice(2, 0, lang);
+    location_parts[0] += '//';
+    window.location = location_parts.join('/');
   }
+};
+
+// Detect browser lang if there is no lang in the URL
+var location_parts = window.location.toString().split('/').filter(function (_) {
+  return _;
+});
+var lang_in_url = '';
+if (supported_langs.indexOf(location_parts[2]) !== -1) {
+  lang_in_url = location_parts[2];
+  location_parts.splice(2, 1);
+}
+if (!lang_in_url) {
+  change_loc(navigator.language.startsWith('fr') ? 'fr' : 'en');
+}
 
 var base_url = location_parts[0] + '//' + location_parts[1] + '/';
